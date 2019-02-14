8f6b80a381c40aaf325f1ec1eea366be6cf2c618
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-02-14 15:38:20 -0500

    Create update/validate func for each input

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -4,33 +4,121 @@ import './AddNote.css'
 import RouteContext from '../RouteContext'
 
 export default class AddNote extends Component {
+
   static defaultProps = {
     folders: [],
   }
+
   static contextType = RouteContext
+
+  static state = {
+    formValid: false,
+    nameValid: false,
+    contentValid: false,
+    folderIdValid: false,
+
+    name: '',
+    content: '',
+    folderId: '',
+
+    errorMessages: {},
+  }
+
+  updateName = (e) => {
+
+    let isValid      = true;
+    let errorMessage = null;
+
+    if (e.target.value.length < 3) {
+      isValid = false;
+      errorMessage = 'Name must be greater than 2 characters in length';
+    }
+
+    if (e.target.value === 'foobar') {
+      isValid = false;
+      errorMessage = 'Name must not be "foobar"'
+    }
+
+    this.setState({
+      name: e.target.value,
+      nameValid: isValid,
+      errorMessages: {
+        name: errorMessage,
+      }
+    });
+  };
+
+  updateContent = (e) => {
+
+    let isValid      = true;
+    let errorMessage = null;
+
+    if (e.target.value.length < 5) {
+      isValid = false;
+      errorMessage = 'Content must be greater than 5 characters in length';
+    }
+
+    if (e.target.value === 'foobar') {
+      isValid = false;
+      errorMessage = 'Content must not be "foobar"'
+    }
+
+    this.setState({
+      content: e.target.value,
+      contentValid: isValid,
+      errorMessages: {
+        content: errorMessage,
+      }
+    });
+  };
+
+  updateFolderId = (e) => {
+
+    let isValid      = true;
+    let errorMessage = null;
+
+    if (!e.target.value) {
+      isValid = false;
+      errorMessage = 'Please choose a folder'
+    }
+
+    this.setState({
+      folderId: e.target.value,
+      folderIdValid: isValid,
+      errorMessages: {
+        folderId: errorMessage,
+      }
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('form submit');
+  }
+
   render() {
     const { folders } = this.context
     return (
       <section className='AddNote'>
         <h2>Create a note</h2>
-        <NotefulForm>
+        <NotefulForm onSubmit={(e) => { this.handleSubmit(e) }}>
           <div className='field'>
             <label htmlFor='note-name-input'>
               Name
             </label>
-            <input type='text' id='note-name-input' />
+            <input type='text' id='note-name-input' onChange={(e) => { this.updateName(e) }}/>
           </div>
           <div className='field'>
             <label htmlFor='note-content-input'>
               Content
             </label>
-            <textarea id='note-content-input' />
+            <textarea id='note-content-input'  onChange={(e) => { this.updateContent(e) }}/>
           </div>
           <div className='field'>
             <label htmlFor='note-folder-select'>
               Folder
             </label>
-            <select id='note-folder-select'>
+            <select id='note-folder-select'  onChange={(e) => { this.updateFolderId(e) }}>
               <option value={null}>...</option>
               {folders.map(folder =>
                 <option key={folder.id} value={folder.id}>
